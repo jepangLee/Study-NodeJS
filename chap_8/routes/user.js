b5b98f520b8dee9f9157d331f074cff49925a1a0
JepangLee <ljsung0805@naver.com>
2019-05-02 20:21:57 +0900

    study chapter_8

---
+++ chap_8/routes/user.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const User = require('../schemas/user');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const users = await User.find({});
+        res.json(users);
+    }
+    catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    const user = new User({
+        name: req.body.name,
+        age: req.body.age,
+        married: req.body.married,
+    });
+
+    try {
+        const result = await user.save();
+        console.log(result);
+        res.status(201).json(result);
+    }
+    catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+module.exports = router;
+
